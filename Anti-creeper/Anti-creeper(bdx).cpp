3c335cf588fd1224e9ff10e19fbe137b8e3b75f8
twoone-3 <62778573+twoone-3@users.noreply.github.com>
2020-08-24 18:27:51 +0800

    update

--- Anti-creeper/Anti-creeper(bdx).cpp
+++ Anti-creeper/Anti-creeper(bdx).cpp
@@ -3,16 +3,16 @@
 
 THook(bool,
 	"?explode@Level@@QEAAXAEAVBlockSource@@PEAVActor@@AEBVVec3@@M_N3M3@Z",
-	void* _this, void* a2, Actor* a3, Vec3 a4, float a5, bool a6, bool a7) {
-	if (a6 != 0) { return original(_this, a2, a3, a4, a5, a6, a7); }
+	void* _this, void* a2, Actor* a3, Vec3 a4, float a5, bool a6, bool a7,float a8,bool a9) {
+	if (a6 != 0 || a5 != 3) { return original(_this, a2, a3, a4, a5, a6, a7,a8,a9); }
 	else {
 		const int id = a3->getEntityTypeId();
 		if (id == 2849) {
 			std::cout << u8"已拦截位于:" << a4.toString() << u8"的爆炸破坏~" << std::endl;
-			return original(_this, a2, a3, a4, a5, a6, 0);
+			return original(_this, a2, a3, a4, a5, a6, 0,a8,a9);
 		}
 		else {
-			return original(_this, a2, a3, a4, a5, a6, a7);
+			return original(_this, a2, a3, a4, a5, a6, a7,a8,a9);
 		}
 	}
 }
--- Anti-creeper/插件模块.cpp
+++ Anti-creeper/插件模块.cpp
@@ -5,17 +5,18 @@ using namespace std;
 
 THook(bool,
 	"?explode@Level@@QEAAXAEAVBlockSource@@PEAVActor@@AEBVVec3@@M_N3M3@Z",
-	void* _this, BlockSource* bs, Actor* a3, const Vec3 pos, float a5, bool a6, bool a7) {
-	if (a6 != 0) { return original(_this, bs, a3, pos, a5, a6, a7); }
+	void* _this, BlockSource* bs, Actor* a3, const Vec3 pos, float a5, bool a6, bool a7, float a8, bool a9) {
+	//cout <<a6<<endl<<a5<<endl;
+	if (a6 != 0 || a5 != 3) { return original(_this, bs, a3, pos, a5, a6, a7, a8, a9); }
 	else {
 		const int id = a3->gettypeid();
 		//cout << id << endl;
 		if (id == 2849) {
 			std::cout << u8"已拦截位于(" << (int)pos.x << "," << (int)pos.y << "," << (int)pos.z << ")" << u8"的爆炸破坏~" << std::endl;
-			return original(_this, bs, a3, pos, a5, a6, 0);
+			return original(_this, bs, a3, pos, a5, a6, 0, a8, a9);
 		}
 		else {
-			return original(_this, bs, a3, pos, a5, a6, a7);
+			return original(_this, bs, a3, pos, a5, a6, a7, a8, a9);
 		}
 	}
 }
