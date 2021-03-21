da3923f4e6659707593056aae744e1de07144bb2
WillowSauceR <63277109+WillowSauceR@users.noreply.github.com>
2021-03-21 19:26:42 +0800

    Update mod.cpp

--- Anti-creeper/mod.cpp
+++ Anti-creeper/mod.cpp
@@ -1,20 +1,20 @@
 #include "../pch.h"
 #include "mod.h"
-static struct Vec3 {
+struct Vec3 {
 	float x, y, z;
 }pos;
 Hook(explode, bool, "?explode@Level@@UEAAXAEAVBlockSource@@PEAVActor@@AEBVVec3@@M_N3M3@Z",
 	void* _this, void* bs, /*Actor*/void* a3, const Vec3 pos, float a5, bool a6, bool a7, float a8, bool a9) {
 	if (a3) {
 		if (SYMCALL<int>("?getEntityTypeId@Actor@@UEBA?AW4ActorType@@XZ", a3) == 2849)
-			std::cout << u8"[Anti-Creeprer]ÒÑÀ¹½ØÎ»ÓÚ: " << pos.x << " " << pos.y << " " << pos.z << u8" µÄ±¬Õ¨ÆÆ»µ" << std::endl;
+			std::cout << u8"[Anti-Creeprer]å·²æ‹¦æˆªä½äºŽ: " << pos.x << " " << pos.y << " " << pos.z << u8" çš„çˆ†ç‚¸ç ´å" << std::endl;
 			a7 = 0;
 	}
 	return original(_this, bs, a3, pos, a5, a6, a7, a8, a9);
 }
 BOOL WINAPI DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved) {
 	if (reason == 1) {
-		puts(u8"[Anti-Creeprer]·À¿àÁ¦ÅÂ²å¼þÒÑ×°ÔØ~");
+		puts(u8"[Anti-Creeprer]é˜²è‹¦åŠ›æ€•æ’ä»¶å·²è£…è½½~");
 	}
 	return TRUE;
 }
